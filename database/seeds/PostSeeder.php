5c2ae2b22a33ac7756d3a48cea9dd3e2e2b34503
Jesper Jarlskov <jesper@jarlskov.dk>
2022-03-31 20:46:47 +0200

    Add missing files

---
+++ database/seeds/PostSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('posts')->insert([
+            'title' => 'New post',
+            'slug' => 'new-post',
+            'text' => 'Super awesome post!',
+            'publish_date' => new DateTime(),
+            'published' => true,
+            'user_id' => 1,
+        ]);
+    }
+}
